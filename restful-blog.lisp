1492710ed526240d7bb42187a3a59e58ba0366c1
Florian Margaine <florian@margaine.com>
2015-05-25 10:47:33 +0200

    Starts handling users, so that permissions will be managed too

--- restful-blog.lisp
+++ restful-blog.lisp
@@ -5,7 +5,7 @@
 ;;; "restful-blog" goes here. Hacks and glory await!
 
 (defclass article (restful:resource)
-  ((slug :is-identifier t)
+  ((slug :is-identifier t :reader slug)
    (title :required t)
    (content :default ""))
   (:metaclass restful:resource-metaclass))
@@ -16,20 +16,45 @@
    (content :required t))
   (:metaclass restful:resource-metaclass))
 
-(hunchentoot:start
- (make-instance 'restful:acceptor
-                :port 4444
-                :resource-definition {
-                  "article" {
-                    :class 'article
-                    :collection 'restful:collection
-                    :storage (make-instance 'restful:memory-storage)
-                    :children {
-                      "comment" {
-                        :class 'comment
-                        :collection 'restful:collection
-                        :storage (make-instance 'restful:memory-storage)
+(defclass user (restful:resource)
+  ((action :is-identifier t :reader action))
+  (:metaclass restful:resource-metaclass))
+
+(defmethod restful:has-permission ((user user) method)
+  ;; Only POST methods available, i.e. custom actions
+  (eq method :post))
+
+(defmethod restful:resource-action ((user user))
+  (cond ((string= (slug user) "login") (handle-login))
+        ((string= (slug user) "logout") (handle-logout))
+        (t (restful::http-error hunchentoot:+http-not-found+))))
+
+(defun handle-login ()
+  t)
+
+(defun handle-logout ()
+  t)
+
+(defun main ()
+  (hunchentoot:start
+   (make-instance 'restful:acceptor
+                  :port 4444
+                  :resource-definition {
+                    "article" {
+                      :class 'article
+                      :collection 'restful:collection
+                      :storage (make-instance 'restful:memory-storage)
+                      :children {
+                        "comment" {
+                          :class 'comment
+                          :collection 'restful:collection
+                          :storage (make-instance 'restful:memory-storage)
+                        }
                       }
                     }
-                  }
-                }))
+                    "user" {
+                      :class 'user
+                      :collection 'restful:collection
+                      :storage t
+                    }
+                  })))
